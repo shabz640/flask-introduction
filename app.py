6a7cc186bc9e2762da430f66366125a7417cd67a
trainee <trainee@factweavers.com>
2021-07-10 20:45:37 +0530

    Added config_file

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
-
 from flask import Flask,render_template,request, jsonify
 from elasticsearch import Elasticsearch
+from read_config import ReadConfig
 
-es_client = Elasticsearch()
+config_file = "config_file.conf"
+
+config = ReadConfig(config_file)
+index_name = config.get_config("config", "index_name")
+host_name = config.get_config("config", "hostname")
+port = config.get_config("config", "port")
+
+es_client = Elasticsearch([host_name])
 app = Flask(__name__)
 @app.route('/useradd', methods = ["POST"])
 def add_user():
     first_name = request.json.get('fname')
     last_name = request.json.get('lname')
+    age = request.json.get('age')
     user_obj = {
         "first_name": first_name,
-        "last_name": last_name
+        "last_name": last_name,
+        "age": age
     }
 
-    result = es_client.index(index="user-map", body=user_obj)
+    result = es_client.index(index=index_name, body=user_obj)
     return jsonify(result)
 
-@app.route('/userdelete', methods = ["DELETE"])
-def delete_user():
-    user_name = request.json.get('uname')
-    result = es_client.delete()
-
-
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=port)
